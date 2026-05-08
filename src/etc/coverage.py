32d61035d48dc19b8080e5f187db4f7a2e2449fe
SamSaladino <sanralisistrata@gmail.com>
2026-05-08 21:05:01 +0200

    chore : create a sampling energy method
    create a method on coverage.py to sample the energy of random subsets of nodes, and use it in
    the notebook to get a better understanding of the energy distribution.

--- src/etc/coverage.py
+++ src/etc/coverage.py
@@ -33,6 +33,46 @@ class Coverage:
 
         return abs(value)
     
+
+    def sample_energy(self, n, k, 
+                               n_samples=1000, 
+                               mu: float = 1.0, gamma: float = 1.0,
+                               seed=42
+                               ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Sample random subsets of nodes and compute their energies 
+        to find the distribution of energy configurations and get 
+        the energy thresholds.
+        
+        Returns:
+        --------
+        energies : np.ndarray
+            Array of energy values for the sampled subsets.
+        min_energy_subset : np.ndarray
+            The subset of nodes with the minimum energy.
+        max_energy_subset : np.ndarray
+            The subset of nodes with the maximum energy.
+        """
+
+        rng = np.random.default_rng(seed)
+        energies = []
+        samples = []
+        S_index = np.zeros(n, dtype=int)
+
+        for _ in range(n_samples):
+
+            nodes_idx = rng.choice(n, size=k, replace=False)
+            
+            S_index[nodes_idx] = 1
+
+            E = self.energy(S_index, mu=mu, gamma=gamma)
+            energies.append(E)
+            samples.append(S_index)
+
+        energies = np.array(energies)
+        imin, imax = energies.argmin(), energies.argmax()
+        return energies, samples[imin], samples[imax]
+    
     # -----------------------------------------------------------#
     # Energy minimization
     # -----------------------------------------------------------#
