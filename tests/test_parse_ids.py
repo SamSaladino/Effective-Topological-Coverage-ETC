0bbd712a302b8d8d2512fd161f8c7abc2dae9f55
SamSaladino <sanralisistrata@gmail.com>
2026-06-26 20:33:43 +0200

    add: unittest for parse ids
    a bug in the chebi dictionary was exposed

---
+++ tests/test_parse_ids.py
@@ -0,0 +1,104 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from etc.parse_ids import XMLParser
+
+@pytest.fixture
+def toy_sbml_file(tmp_path):
+    sbml_content = """<?xml version="1.0" encoding="UTF-8"?>
+<sbml
+    xmlns="http://www.sbml.org/sbml/level3/version1/core"
+    xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+    level="3"
+    version="1">
+
+    <model id="toy_model">
+        <listOfCompartments>
+            <compartment id="c" constant="true"/>
+        </listOfCompartments>
+
+        <listOfSpecies>
+            <species
+                id="M_glucose_c"
+                compartment="c"
+                boundaryCondition="false"
+                hasOnlySubstanceUnits="false"
+                constant="false">
+
+                <annotation>
+                    <rdf:RDF>
+                        <rdf:Description>
+                            <rdf:Bag>
+                                <rdf:li
+                                    rdf:resource="https://identifiers.org/chebi/CHEBI:17634"/>
+                                <rdf:li
+                                    rdf:resource="https://identifiers.org/kegg.compound/C00031"/>
+                                <rdf:li
+                                    rdf:resource="https://identifiers.org/hmdb/HMDB0000122"/>
+                                <rdf:li
+                                    rdf:resource="https://identifiers.org/metanetx.chemical/MNXM41"/>
+                            </rdf:Bag>
+                        </rdf:Description>
+                    </rdf:RDF>
+                </annotation>
+            </species>
+
+            <species
+                id="M_unknown_c"
+                compartment="c"
+                boundaryCondition="false"
+                hasOnlySubstanceUnits="false"
+                constant="false"/>
+        </listOfSpecies>
+    </model>
+</sbml>
+"""
+
+    path = tmp_path / "toy_model.xml"
+    path.write_text(sbml_content, encoding="utf-8")
+
+    return path
+
+def test_to_identifier_df_extracts_expected_identifiers(
+    toy_sbml_file,
+):
+    parser = XMLParser(str(toy_sbml_file))
+
+    identifier_df = parser.to_identifier_df()
+
+    assert isinstance(identifier_df, pd.DataFrame)
+
+    assert list(identifier_df.index) == [
+        "M_glucose_c",
+        "M_unknown_c",
+    ]
+
+    assert identifier_df.loc["M_glucose_c", "chebi"] == "17634"
+    assert identifier_df.loc["M_glucose_c", "kegg"] == "C00031"
+    assert identifier_df.loc["M_glucose_c", "hmdb"] == "HMDB0000122"
+
+    assert (
+        identifier_df.loc["M_glucose_c", "metanetx"]
+        == "MNXM41"
+    )
+
+    assert pd.isna(
+        identifier_df.loc["M_unknown_c", "chebi"]
+    )
+
+    assert pd.isna(
+        identifier_df.loc["M_unknown_c", "kegg"]
+    )
+
+def test_get_chebi_numbers_uses_human1_ids_as_keys(
+    toy_sbml_file,
+):
+    parser = XMLParser(str(toy_sbml_file))
+    parser.extract_data()
+
+    chebi_mapping = parser.get_chebi_numbers()
+
+    assert chebi_mapping == {
+        "M_glucose_c": "17634",
+    }
